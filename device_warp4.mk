128bb128770dfaa6c605d76f3c28a78e741d497f
superr <superr@ddayweb.com>
2015-05-27 01:13:50 -0500

    bringup for TWRP 5.0 branch

--- device_warp4.mk
+++ device_warp4.mk
@@ -17,6 +17,10 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# DT Image
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/dt.img:dt.img \
+    
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
